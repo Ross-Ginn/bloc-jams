ecd8582678d941341a6525ff6b6d9112c75349f1
Ross <rosswginn@gmail.com>
2017-05-31 21:28:57 -0700

    fixes 9-11

--- scripts/album.js
+++ scripts/album.js
@@ -29,6 +29,22 @@ var albumPicasso = {
      ]
  };
 
+ // Another Example Album
+ var albumSmashingPumpkins = {
+     title: 'Siamese Dream',
+     artist: 'The Smashing Pumpkins',
+     label: 'Triclops',
+     year: '1993',
+     albumArtUrl: 'assets/images/album_covers/SmashingPumpkins-SiameseDream.jpg',
+     songs: [
+         { title: 'Rocket Boy', duration: '1:01' },
+         { title: 'Today', duration: '5:01' },
+         { title: 'Cherb Rock', duration: '3:21'},
+         { title: 'Space Boy?', duration: '3:14' },
+         { title: 'Soma', duration: '2:15'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -41,29 +57,36 @@ var albumPicasso = {
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-     // #2
-     albumTitle.firstChild.nodeValue = album.title;
+ var setCurrentAlbum = function(album) {
+
+     albumTitle.firstChild.nodeValue = album.name;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
 
-     // #3
      albumSongList.innerHTML = '';
 
-     // #4
      for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].length);
      }
  };
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumSmashingPumpkins];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+       setCurrentAlbum(albums[index]);
+       index++;
+       if (index == albums.length) {
+         index = 0;
+       }
+    });
  };
--- scripts/landing.js
+++ scripts/landing.js
@@ -17,9 +17,11 @@ window.onload = function() {
         animatePoints(pointsArray);
     }
 
-  window.addEventListener('scroll', function(event) {
-    if (pointsArray[0].getBoundingClientRect().top <= 500) {
-               animatePoints(pointsArray);
-           }
-});
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);
+      }
+    });
 }
